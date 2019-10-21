5a57750dd0831c7657e04b3bb27178c1132a5fef
Almir Arruda <almir.arruda@novatendencia.localdomain>
2019-10-21 13:54:48 -0300

    endpoint cliente

--- src/main/java/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -1,20 +1,20 @@
 package com.example.cursomc.services;
 
-import com.example.cursomc.domain.Categoria;
-import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 
     @Autowired
-    private CategoriaRepository repo;
+    private ClienteRepository repo;
 
-    public Categoria buscar (Integer id) {
-        Optional <Categoria> obj = repo.findById(id);
+    public Cliente buscar (Integer id) {
+        Optional <Cliente> obj = repo.findById(id);
         return obj.orElse(null);
     }
 }
